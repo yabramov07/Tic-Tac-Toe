8e28ca760ed4e944018ace37c6f000155f8cf08c
yabramov <yabramov07@gmail.com>
2024-03-30 23:19:03 +0300

    исправлена ошибка с первой буквой для игрока

--- TicTacToeBot.py
+++ TicTacToeBot.py
@@ -3,7 +3,7 @@ from random import randint
 
 with open('BotFile.txt', 'r', encoding='utf-8') as f:
     data = f.read().split('\n')
-final = None
+final = False
 used = []
 
 bot = telebot.TeleBot('6735978760:AAEDgNkzBJY0rXrLrN8cz12BM9tCNMbK3gk')
@@ -11,26 +11,27 @@ bot = telebot.TeleBot('6735978760:AAEDgNkzBJY0rXrLrN8cz12BM9tCNMbK3gk')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот, для которого еще не придумали конкретного применения, рад поработать на вас, "
-                          "но не забывайте - 'время - деньги'")
+    bot.reply_to(message, "Привет! Я бот для игры в города, напиши первый город и я продолжу \nДля "
+                          "получения более подробной информации используйте /help")
     used.clear()
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.reply_to(message, "здесь могла быть ваша реклама")
+    bot.reply_to(message, "функция /clear позволяет начать игру с начала")
 
 
 @bot.message_handler(commands=['clear'])
 def clean(message):
     used.clear()
-    bot.reply_to(message, 'игра начинается заново')
+    bot.reply_to(message, 'список использованных городов очищен, игра начинается заново')
 
 
 def examination(message, st):
     if st in data:
         if st not in used:
-            if st[0] == final or final is None:
+            print(final)
+            if st[0] == final or not final:
                 return True
             else:
                 bot.reply_to(message, '<<город должен начинаться на "' + str(final) + '">>')
@@ -57,10 +58,12 @@ def last_and_initial(st):
 
 @bot.message_handler(content_types=['text'])
 def texting(message):
+    global final
     st = message.text.lower()
     if examination(message, st):
         used.append(st)
         final = last_and_initial(st)
+        print(final)
         t = []
         for elem in data:
             if elem[0] == final:
@@ -68,6 +71,7 @@ def texting(message):
         # print(t)
         city = t[randint(0, len(t) - 1)]
         final = last_and_initial(city)
+        print(final)
         used.append(city)
         bot.send_message(message.from_user.id, city)
 
